1fae7b26613f28aeb8f077f25fa304c437a423cc
Ankylotech <jonas.rubeck@gmail.com>
2020-07-03 15:44:43 +0200

    size changes

--- src/site.js
+++ src/site.js
@@ -1,10 +1,11 @@
-
+import 'react-app-polyfill/ie9'
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
 require('./common')
 require('./images/logo.png')
 require('./images/radar_legend.png')
 require('./gtm.js')
 
 const GoogleSheetInput = require('./util/factory')
-
 
 GoogleSheetInput().build()
