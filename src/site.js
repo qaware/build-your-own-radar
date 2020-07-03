462be1e69fcc6c00d87f7e6031dfdaae762c462f
Ankylotech <jonas.rubeck@gmail.com>
2020-07-03 16:33:52 +0200

    Revert "size changes"
    This reverts commit 1fae7b26

--- src/site.js
+++ src/site.js
@@ -1,11 +1,10 @@
-import 'react-app-polyfill/ie9'
-import 'react-app-polyfill/ie11'
-import 'react-app-polyfill/stable'
+
 require('./common')
 require('./images/logo.png')
 require('./images/radar_legend.png')
 require('./gtm.js')
 
 const GoogleSheetInput = require('./util/factory')
+
 
 GoogleSheetInput().build()
