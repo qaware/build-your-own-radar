7ca7dba41e36e83be11313f11c4241b90999fb6e
Ankylotech <jonas.rubeck@gmail.com>
2020-07-03 14:07:43 +0200

    made selected radar smaller

--- src/graphing/radar.js
+++ src/graphing/radar.js
@@ -510,7 +510,7 @@ const Radar = function (size, radar) {
     d3.selectAll('.quadrant-table.' + order).classed('selected', true)
     d3.selectAll('.blip-item-description').classed('expanded', false)
 
-    var scale = 2
+    var scale = 1.5
 
     var adjustX = Math.sin(toRadian(startAngle)) - Math.cos(toRadian(startAngle))
     var adjustY = Math.cos(toRadian(startAngle)) + Math.sin(toRadian(startAngle))
