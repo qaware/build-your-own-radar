7a910abab9f7948566cc3e17a1f5f5174b30f0a9
Ankylotech <jonas.rubeck@gmail.com>
2020-07-03 16:37:52 +0200

    size changed

--- src/graphing/radar.js
+++ src/graphing/radar.js
@@ -515,7 +515,9 @@ const Radar = function (size, radar) {
     d3.selectAll('.blip-item-description').classed('expanded', false)
 
     var scale = 2
+    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification))
 
+    if (isSafari) scale /= 3
 
     var adjustX = Math.sin(toRadian(startAngle)) - Math.cos(toRadian(startAngle))
     var adjustY = Math.cos(toRadian(startAngle)) + Math.sin(toRadian(startAngle))
