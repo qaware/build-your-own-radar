3b3476d3d0154b86b7dd5d8966d27aef02a35f05
Ankylotech <jonas.rubeck@gmail.com>
2020-07-03 11:59:38 +0200

    removed kaniko cache, changed thoughtworks links to qaware

--- src/util/factory.js
+++ src/util/factory.js
@@ -259,7 +259,7 @@ function plotLoading (content) {
 function plotLogo (content) {
   content.append('div')
     .attr('class', 'input-sheet__logo')
-    .html('<a href="https://www.thoughtworks.com"><img src="/images/tw-logo.png" / ></a>')
+    .html('<a href="https://www.qaware,de"><img src="/images/tw-logo.png" / ></a>')
 }
 
 
