17e031002536d60b3c595077c50f021db073867b
Ankylotech <jonas.rubeck@gmail.com>
2020-07-07 11:08:09 +0200

    updating Cloud-function on git-push

---
+++ index.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const { Parser } = require("json2csv");
+var fields = ['name','quadrant','ring','isNew','description']
+exports.getSourceAsCsv = functions.https.onRequest((request, response) => {
+    if (request.method === 'OPTIONS') {
+      // Send response to OPTIONS requests
+      response.set('Access-Control-Allow-Methods', 'GET');
+      response.set('Access-Control-Allow-Headers', 'Authorization');
+      response.set('Access-Control-Max-Age', '3600');
+      response.status(204).send('');
+    }
+    response.set('Access-Control-Allow-Origin', '*');
+    var jsondata = [];
+    const db = admin.firestore();
+    const dBData = db.collection("radar-data").orderBy("ring", "asc");
+    
+    return dBData.get().then((querySnapshot) => {
+        querySnapshot.forEach(doc => {
+            jsondata.push(doc.data());
+        });
+        var json2csv = new Parser({delimiter: ';',fields: fields});
+        const csv = json2csv.parse(jsondata);
+        response.setHeader(
+            "Content-disposition",
+            "attachment; filename=files.csv"//file name.csv
+        );
+        response.set("Content-Type", "text/csv");
+        return response.status(200).send(csv)
+    }).catch((err) => {
+        return console.log(err);
+    }); 
+});
