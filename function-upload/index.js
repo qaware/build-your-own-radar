0487e968a95949356c0042c1730c067a0f72128c
Ankylotech <jonas.rubeck@gmail.com>
2020-07-07 17:53:28 +0200

    bug fixing

--- function-upload/index.js
+++ function-upload/index.js
@@ -15,9 +15,10 @@ exports.uploadCSV = functions.https.onRequest((request, response) => {
 
       // Too much POST data, kill the connection!
       // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-      if (body.length > 1e6)
+      if (body.length > 1e6) {
         request.connection.destroy()
-    });
+      }
+    })
 
     request.on('end', function () {
       var post = qs.parse(body)
@@ -26,7 +27,7 @@ exports.uploadCSV = functions.https.onRequest((request, response) => {
   }
   const db = admin.firestore()
   var keys = Object.keys(jsondata)
-  var version = db.collection('version').doc('currentVersion')
+  const version = db.collection('version').doc('currentVersion')
   const res = version.update({
     version: admin.firestore.FieldValue.increment(1)
   })
