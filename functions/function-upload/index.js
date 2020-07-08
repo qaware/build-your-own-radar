4d230f135c895e0099aeb3f1b5150c1692afd046
Ankylotech <jonas.rubeck@gmail.com>
2020-07-08 16:46:59 +0200

    functioning storage csv upload

--- functions/function-upload/index.js
+++ functions/function-upload/index.js
@@ -1,23 +1,27 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
-const { Storage } = require('@google-cloud/storage');
-const csvtojson = require('csvtojson');
-
-const db = admin.firestore();
-
-exports.uploadSourceFromStorage = (event, context,callback) => {
-  const storage = new Storage();
-  const bucket = storage.bucket('techradar-versiondata');
+const csvtojson = require('csvtojson')
+const { Storage } = require('@google-cloud/storage')
+const db = admin.firestore()
+exports.uploadSourceFromStorage = (event, context) => {
+  const storage = new Storage()
+  const bucket = storage.bucket('techradar-versiondata')
 
   bucket.file(event.name).download()
     .then(data => {
-
-      // Do something with the contents constant, e.g. derive the value you want to write to Firestore
-      var jsondata = csvtojson().fromFile(data[0])
-      for(var item in jsondata){
-        db.collection(event.name.replace('.csv','')).doc().set(item);
-      }
-    });
-  return callback();
-};
+      const csv = data[0].toString()
+      csvtojson({ delimiter: [';', ','] }).fromString(csv).then((jsonObj) => {
+        jsonObj.forEach(function (obj) {
+          console.log(obj)
+          db.collection(event.name.replace('.csv', '')).add(obj).then(function (docRef) {
+            console.log('Document written with ID: ', docRef.id)
+          })
+            .catch(function (error) {
+              console.error('Error adding document: ', error)
+            })
+        })
+      })
+      db.collection('version').doc('currentVersion').set({ version: event.name.replace('.csv', '') })
+    })
+}
