7df6ad0d6c1b1d4fa324bb4e311ad1488a93d19d
Ankylotech <jonas.rubeck@gmail.com>
2020-07-08 10:54:53 +0200

    bug fixing

--- function-upload/index.js
+++ function-upload/index.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
 const csv = require('csvtojson')
 exports.uploadSourceFromStorage = (event, context) => {
-  const db = admin.db()
+  const db = admin.firestore()
   var data = csv.fromFile(event.name)
   for (var item in data) {
     db.collection(event.name.replace('.csv', '')).add(item)
