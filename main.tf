ca5d531205bbb90b3a20ea2abeef32aab2a9a1e3
Ankylotech <jonas.rubeck@gmail.com>
2020-07-09 17:04:23 +0200

    adding Terraform infrastructure-as-code

---
+++ main.tf
@@ -0,0 +1,37 @@
+#use GOOGLE_APPLICATION_CREDENTIALS environment variable for credentials JSON
+provider "google-beta" {
+  project = "qaware-techradar"
+  region  = "europe-west1"
+}
+
+provider "google" {
+  project = "qaware-techradar"
+  region  = "europe-west1"
+}
+
+resource "google_cloudbuild_trigger" "github-trigger" {
+  provider = google-beta
+  filename = "cloudbuild.yaml"
+  github {
+    owner = "qaware"
+    name   = "build-your-own-radar"
+    push {
+    	branch = "master"
+    }
+  }
+  
+}
+
+resource "google_storage_bucket" "static-site" {
+  name          = "techradar-versiondata"
+  location      = "EU"
+  force_destroy = true
+  project = "qaware-techradar"
+
+}
+
+
+
+
+
+
